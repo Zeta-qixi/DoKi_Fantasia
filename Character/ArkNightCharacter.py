3d7d6efd1d9d96a8c945e686fa2c2f9705e2ea27
Zeta-qixi <1019289695@qq.com>
2025-03-06 03:48:13 +0800

    add template

--- Character/ArkNightCharacter.py
+++ Character/ArkNightCharacter.py
@@ -2,6 +2,19 @@ import re
 from typing import Dict, List
 from bs4 import BeautifulSoup
 
+base_template =  """
+## 基本信息
+- 作品：明日方舟
+- 角色名称：{}
+- 性别：{}
+- 对用户称呼：博士
+"""
+
+
+voice_template ="""
+## 语音
+{}
+"""
 class Character_Info:
     # 根据属性名设计的参数列表
     params = {
@@ -24,7 +37,11 @@ class Character_Info:
         self.race = race
         self.height = height
         self.infection_status = infection_status
-        self.archive = archive # 档案 1 - 4 + 晋升记录
+        self.archive = archive # 档案
+        self.voice = ''
+        
+    def add_voice(self, v):
+        self.voice = v
     
     @classmethod
     def init_for_wiki(cls, soup: BeautifulSoup):
@@ -40,8 +57,10 @@ class Character_Info:
         raw_texts = []
         for div in soup.find_all('div', {'class': "poem"}):
             raw_texts.append(div.text)
-            
-        base_info = raw_texts[6].strip()
+        
+        for _i, text in enumerate(raw_texts):
+            if '【代号' in text: break
+        base_info = raw_texts[_i].strip()
         pattern = r'【(.*?)】(.*?)(?=【|$)'
         matches = re.findall(pattern, base_info, flags=re.DOTALL)
         result = {}
@@ -52,32 +71,47 @@ class Character_Info:
                 result[attr_key] = cleaned_value
         
         result['archive'] = []
-        for i in range(14,24,2):
-            result['archive'].append(raw_texts[i].strip())
+        for i, text in enumerate(raw_texts):
+            if '档案资料' in text:
+                result['archive'].append(raw_texts[i+1].strip())
+   
+            
         
         return cls(**result)
-    
 
+    def get_base_info(self):
+        return base_template.format(self.name, self.gender)
     
-class Character_Voice:
+    def get_archive_info(self):
+        num = ['一', '二', '三', '四', '五']
+        text = '\n'.join([f'- 剧情{num[i]}：{t}' for i, t in enumerate(self.archive)])
+        return f"\n{text}\n"
+            
+    
+    
+class Character_Story:
     ...
 
-class Character_Story:
+class Character_Voice:
     
-    def __init__(self, data: List[Dict[str, str]]):
+    def __init__(self, data: Dict[str, str]):
         self.voice = data
     
     @classmethod
     def init_for_wiki(cls, soup: BeautifulSoup):
         voice_soup = soup.find('div',{'data-toc-title':'语音记录'})
         voice_soup.find_all('div',{'class':'voice-data-item'})[0]['data-title']
-        voice_data = []
+        voice_data = {}
         for v in voice_soup.find_all('div',{'class':'voice-data-item'}):
-            voice_data.append(
-                {v['data-title'].strip(): v.find('div').text}
-            )
-        
+            
+            if (k:= v['data-title'].strip()) not in ['标题']:
+                voice_data[k] = v.find('div').text
+            
         return cls(voice_data)
+
+    def get_voice_info(self):
+        
+        return voice_template.format('\n'.join([f"- {v}" for v in self.voice.values()]))
     
 
 class Character(Character_Info, Character_Story, Character_Voice):
